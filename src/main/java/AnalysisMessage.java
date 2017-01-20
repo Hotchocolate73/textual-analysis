e9ca0b4418f8dc7712975308f4f26cc98768a133
Hotchocolate73 <dylan_mcneil@hotmail.co.uk>
2017-01-20 22:42:34 +0000

    Character map created, can now read from file, can count number of characters as well as store them on a map. Tests created.

--- src/main/java/AnalysisMessage.java
+++ src/main/java/AnalysisMessage.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * Created by Dylan on 20/01/2017.
  */
 public class AnalysisMessage
 {
     private String message;
+    private HashMap<Character, Integer> characterMap;
+
+    public HashMap<Character, Integer> getCharacterMap()
+    {
+        return characterMap;
+    }
+
+    public void setCharacterMap(HashMap<Character, Integer> characterMap)
+    {
+        this.characterMap = characterMap;
+    }
 
     public AnalysisMessage(){
         message = "";
@@ -19,7 +35,59 @@ public class AnalysisMessage
         this.message = message;
     }
 
-    public void createMessageFromFile(){
+    public void createMessageFromFile() throws IOException
+    {
+        FileReader fr = new FileReader("D:\\Users\\Dylan\\OneDrive\\Documents\\Programming\\Java\\textual-analysis2\\text files\\jekyll.txt");
+        BufferedReader textReader = new BufferedReader(fr);
+
+        String finalOutput = "";
+        int counter = 1;
+        String currentLine;
+
+        while((currentLine = textReader.readLine()) != null){
+            finalOutput += currentLine + "\n";
+            counter++;
+        }
+
+        textReader.close();
+        this.message = finalOutput;
+    }
+
+    public int countWords()
+    {
+        int wordCount = 0;
+        for(int currentChar = 0; currentChar < message.length(); currentChar++){
+            if(message.charAt(currentChar) == ' '){
+                wordCount++;
+            }
+        }
+
+        wordCount++;
+        return wordCount;
+    }
+
+    public int countCharacterOccurrences()
+    {
+        characterMap = new HashMap<Character, Integer>();
+
+        int numberOfCharacters = 0;
+        for(int currentCharacter = 0; currentCharacter < message.length(); currentCharacter++){
+            if(characterMap.containsKey(message.charAt(currentCharacter)))
+            {
+                characterMap.put(message.charAt(currentCharacter), characterMap.get(message.charAt(currentCharacter)) + 1);
+            }
+            else
+            {
+                characterMap.put(message.charAt(currentCharacter), 1);
+            }
+
+            numberOfCharacters++;
+        }
+
+        return numberOfCharacters;
+    }
+
+    public void printCharacterMap(){
 
     }
 }
--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -1,5 +1,9 @@
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
@@ -8,11 +12,35 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class InputTest
 {
     @Test
-    public void createMessageFromFileSuccessfully(){
+    @Ignore
+    public void createMessageFromFileSuccessfully() throws IOException
+    {
         AnalysisMessage message = new AnalysisMessage();
 
         message.createMessageFromFile();
 
         assertNotEquals(message.getMessage(), "");
+        //System.out.println(message.getMessage());
+        System.out.println("Words: " + message.countWords());
+        System.out.println("Characters: " + message.countCharacterOccurrences());
+    }
+
+    @Test
+    public void countWordsInStringTest(){
+        AnalysisMessage message = new AnalysisMessage();
+        message.setMessage("one two three");
+
+        int numberOfWords = message.countWords();
+
+        assertEquals(numberOfWords, 3);
+    }
+
+    @Test
+    public void checkIfStringCountsEscapeCharacters(){
+        AnalysisMessage message = new AnalysisMessage();
+
+        message.setMessage("Hi\n");
+        int characterCount = message.countCharacterOccurrences();
+        assertEquals(characterCount, 3);
     }
 }
